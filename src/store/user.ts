8154db173e741282c6398fd711094a34891e1981
Luis De La Cruz <luisdanieldlcg@gmail.com>
2024-10-17 14:59:29 -0400

    add user store

---
+++ src/store/user.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+export interface User {
+  userId: number;
+  email: string;
+  username: string;
+}
+
+type UserStore = {
+  user: User | null;
+  setUser: (user: User) => void;
+};
+
+export const useUserStore = create<UserStore>((set) => ({
+  user: null,
+  setUser: (user) => {
+    set({ user });
+  },
+}));
